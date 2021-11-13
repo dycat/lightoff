68dc438ee17fa19f8beb044dd61265ce761138ea
yang <yang@192.168.0.104>
2021-11-13 21:33:06 +0800

    initial scripts

---
+++ lightoff.py
@@ -0,0 +1,16 @@
+import subprocess
+
+class LightController:
+    def turn_off(self):
+        subprocess.run("echo 0 | sudo tee  /sys/class/leds/led0/brightness", shell=True)
+        subprocess.run("echo 0 | sudo tee /sys/class/leds/led1/brightness", shell=True)
+
+    def turn_on(self):
+        subprocess.run("echo 0 | sudo tee  /sys/class/leds/led0/brightness", shell=True)
+        subprocess.run("echo 0 | sudo tee /sys/class/leds/led1/brightness", shell=True)
+
+
+
+if __name__ == "__main__":
+    light_contrl = LightController()
+    light_contrl.turn_off()
